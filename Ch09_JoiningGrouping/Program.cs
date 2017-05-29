75ea395a722c04bf2330c03158b04a3d481ebd04
elawa <elawa@LAPTOP-P42C8U26>
2017-05-29 11:27:12 +0100

    LINQ join, grouping, projection

---
+++ Ch09_JoiningGrouping/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch09_JoiningGrouping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Northwind();
+
+            var categories = db.Categories.Select(c => new { c.CategoryID, c.CategoryName }).ToArray();
+            var products = db.Products.Select(p => new { p.ProductID, p.ProductName, p.CategoryID }).ToArray();
+
+            // join every product to its category 
+            var queryJoin = categories.Join(products, category => category.CategoryID, product => product.CategoryID,
+                (c, p) => new { c.CategoryName, p.ProductName, p.ProductID })
+                .OrderBy(cp => cp.ProductID);
+
+            foreach (var item in queryJoin)
+            {
+                WriteLine($"{item.ProductID}: {item.ProductName} is in {item.CategoryName}");
+            }
+
+            // group all products by their category
+            var queryGroup = categories.GroupJoin(products,
+                category => category.CategoryID,
+                product => product.CategoryID,
+                (c, Products) => new { c.CategoryName, Products = Products.OrderBy(p => p.ProductName) });
+
+            foreach (var item in queryGroup)
+            {
+                WriteLine($"{item.CategoryName} has {item.Products.Count()} products.");
+                foreach (var product in item.Products)
+                {
+                    WriteLine($" {product.ProductName}");
+                }
+            }
+
+        }
+    }
+}
--- Ch09_LINQ/Program.cs
+++ Ch09_LINQ/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Ch09_LINQ
 {
@@ -10,6 +11,20 @@ namespace Ch09_LINQ
     {
         static void Main(string[] args)
         {
+            var names = new string[] { "Pamela", "Jimmy", "Toby", "Ala", "Kurt", "Ola", "Ewa", "Tonia" };
+            var query = names
+                .Where(name => name.Length > 3)
+                .OrderBy(name => name.Length)
+                .ThenBy(name => name);
+            foreach (var item in query)
+            {
+                WriteLine(item);
+            }
+        }
+
+        static bool NameLongerThanFour(string name)
+        {
+            return name.Length > 4;
         }
     }
 }
